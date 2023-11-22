794cac9bc8bfa1b674e9929773aead8ff7fbbdb2
Josh Booker <josh.booker2@gmail.com>
2023-11-22 15:05:01 +0000

    configured note controller receive post requests

--- server/src/main/java/com/noteApp/noteService/controllers/NoteController.java
+++ server/src/main/java/com/noteApp/noteService/controllers/NoteController.java
@@ -3,6 +3,7 @@ package com.noteApp.noteService.controllers;
 import com.noteApp.noteService.repositories.NoteRepository;
 import com.noteApp.noteService.models.Note;
 import com.noteApp.noteService.models.User;
+import com.noteApp.noteService.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,8 @@ import java.util.Optional;
 public class NoteController {
     @Autowired
     NoteRepository noteRepository;
+    @Autowired
+    UserRepository userRepository;
     @GetMapping(value = "/notes")
     public ResponseEntity<List<Note>> getAllNotesForUser(
             @RequestParam(name="email") String email){
@@ -25,15 +28,24 @@ public class NoteController {
         return new ResponseEntity<>(noteRepository.findById(id), HttpStatus.OK);
     }
     @PostMapping(value = "/notes")
-    public void createNote(@RequestBody Note newNote) {
+    public ResponseEntity<?> createNote(@RequestBody Note newNote, @RequestParam(name = "email") String email) {
+        System.out.println("new note title");
+        System.out.println(newNote.getTitle());
+        System.out.println("new note content");
+        System.out.println(newNote.getNoteContent());
         Note note = new Note(newNote.getTitle(), newNote.getNoteContent());
+        User user = userRepository.findByEmail(email);
+        user.getNotes().add(note);
+        note.setUser(user);
         noteRepository.save(note);
-    };
-    @DeleteMapping(value = "notes/{id}")
+        return new ResponseEntity<>(note, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/notes/{id}")
     public void deleteNote(@PathVariable Long id) {
         noteRepository.deleteById(id);
     };
-    @PutMapping(value = "notes/{id}")
+    @PutMapping(value = "/notes/{id}")
     public Note editNote(@PathVariable Long id, @RequestBody Note newNote) {
         return noteRepository.findById(id).map(note -> {
             note.setTitle(newNote.getTitle());
