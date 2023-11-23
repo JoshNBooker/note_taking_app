53c26249adf0265d4c8c21ad15366183337b89b8
Josh Booker <josh.booker2@gmail.com>
2023-11-23 19:05:15 +0000

    app now correctly sending updated notes to backend and storing them

--- client/src/components/StickyNote.tsx
+++ client/src/components/StickyNote.tsx
@@ -2,39 +2,67 @@ import { ChangeEvent, useEffect, useState, useRef } from 'react';
 import Paragraph from './Content';
 import Paper from './Paper';
 import Title from './Title';
+import { Note } from '../Types';
 
 interface StickyNoteProps {
-	title: string;
-	noteContent: string;
-	setNoteTitle: Function;
-	setNoteContent: Function;
+	originalTitle: string;
+	originalNoteContent: string;
+	setNewNoteTitle: Function;
+	setNewNoteContent: Function;
+	setNoteToEdit: Function;
+	handleEditNote: Function;
+	note: Note;
+	newNoteTitle: string | undefined;
+	newNoteContent: string | undefined;
 }
 
 function StickyNote({
-	title,
-	noteContent,
-	setNoteTitle,
-	setNoteContent,
+	originalTitle,
+	originalNoteContent,
+	setNewNoteTitle,
+	setNewNoteContent,
+	setNoteToEdit,
+	handleEditNote,
+	note,
+	newNoteTitle,
+	newNoteContent,
 }: StickyNoteProps) {
 	const [editMode, setEditMode] = useState(false);
 	const wrapperRef = useRef<HTMLDivElement | null>(null);
 
+	const handleEditClick = () => {
+		setEditMode(true);
+		setNoteToEdit(note);
+	};
+
 	const handleTitleEdit = (e: ChangeEvent<HTMLInputElement>) => {
-		setNoteTitle(e.target.value);
+		e.preventDefault;
+		setNewNoteTitle(e.target.value);
 		console.log('new title: ', e.target.value);
 	};
 
 	const handleContentEdit = (e: ChangeEvent<HTMLInputElement>) => {
-		setNoteContent(e.target.value);
+		e.preventDefault;
+		setNewNoteContent(e.target.value);
 		console.log('new contents', e.target.value);
 	};
+
+	useEffect(() => {
+		handleEditNote();
+		setNewNoteContent(undefined);
+		setNewNoteTitle(undefined);
+	}, [editMode]);
 
 	const handleClickOutside = (event: MouseEvent) => {
 		if (
 			wrapperRef.current &&
-			!wrapperRef.current.contains(event.target as Node)
+			!wrapperRef.current.contains(event.target as Node) &&
+			editMode
 		) {
+			console.log('title in click outside:', newNoteTitle);
+			console.log('noteContent in click outside:', newNoteContent);
 			setEditMode(false);
+			console.log('exited edit mode');
 		}
 	};
 
@@ -43,30 +71,33 @@ function StickyNote({
 		return () => {
 			document.removeEventListener('mousedown', handleClickOutside);
 		};
-	}, []);
+	}, [editMode]);
 
 	return (
 		<div className="note-handle m-5" ref={wrapperRef}>
 			<Paper>
-				<div onClick={() => setEditMode(true)}>
+				{!editMode ? (
+					<div onClick={handleEditClick}>
+						<Title>{originalTitle}</Title>
+					</div>
+				) : (
+					<input
+						type="text"
+						placeholder="Enter title"
+						onChange={handleTitleEdit}
+						defaultValue={originalTitle}
+					/>
+				)}
+
+				<div onClick={handleEditClick}>
 					{!editMode ? (
-						<Title>{title}</Title>
+						<Paragraph>{originalNoteContent}</Paragraph>
 					) : (
 						<input
 							type="text"
-							placeholder={title}
-							onChange={handleTitleEdit}
-						/>
-					)}
-				</div>
-				<div onClick={() => setEditMode(true)}>
-					{!editMode ? (
-						<Paragraph>{noteContent}</Paragraph>
-					) : (
-						<input
-							type="text"
-							placeholder={noteContent}
+							placeholder="Enter content"
 							onChange={handleContentEdit}
+							defaultValue={originalNoteContent}
 						/>
 					)}
 				</div>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from '../context/auth-context.tsx';
@@ -6,11 +5,9 @@ import App from './App.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<AuthProvider>
-				<App />
-			</AuthProvider>
-		</BrowserRouter>
-	</React.StrictMode>
+	<BrowserRouter>
+		<AuthProvider>
+			<App />
+		</AuthProvider>
+	</BrowserRouter>
 );
